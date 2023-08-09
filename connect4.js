5e2b959231d4a2185127c6dd2a34947979e9d5ab
Kelvin <kelzlaur34@gmail.com>
2023-08-09 15:17:20 -0400

    updated code

--- connect4.js
+++ connect4.js
@@ -1,4 +1,4 @@
-console.log('project 1')
+// console.log('project 1')
 
 // Setup variables to access classes and ids to prepare functionalities
 const board = document.querySelector(".board");
@@ -7,3 +7,23 @@ const startScreen = document.querySelector(".startScreen");
 const startButton = document.getElementById("start-Game");
 const winnerMessage = document.getElementById("winner-message");
 // console.log(board);
+// console.log(playerTurn);
+// console.log(startScreen);
+// console.log(startButton);
+// console.log(winnerMessage);
+
+// Setup game object, not working turned into array
+let game = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+]
+// console.log(game)
+
+// Adding window.onload to load game after DOM content is loaded 
+window.onload = startGame = async () => {
+
+}
