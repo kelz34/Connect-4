0a1836385f6bab1759a37db57221331559ccfa92
Kelvin <kelzlaur34@gmail.com>
2023-08-09 13:31:59 -0400

    updated project

---
+++ connect4.js
@@ -0,0 +1,9 @@
+console.log('project 1')
+
+// Setup variables to access classes and ids to prepare functionalities
+const board = document.querySelector(".board");
+const playerTurn = document.getElementById("playerTurn");
+const startScreen = document.querySelector(".startScreen");
+const startButton = document.getElementById("start-Game");
+const winnerMessage = document.getElementById("winner-message");
+// console.log(board);
